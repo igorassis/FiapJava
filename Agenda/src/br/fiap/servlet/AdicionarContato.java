3fb9ff4e175606d5ea3e4a146691166955335ad7
logonrm <logonrm@fiap.com.br>
2017-09-27 22:37:10 -0300

    Add Error e 404

--- Agenda/src/br/fiap/servlet/AdicionarContato.java
+++ Agenda/src/br/fiap/servlet/AdicionarContato.java
@@ -8,12 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.fiap.contato.Contato;
+import br.fiap.dao.ContatoDAO;
+
 @WebServlet("/adicionarContato")
 public class AdicionarContato extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String endereco = request.getParameter("endereco");
+		String dtaNasc = request.getParameter("dtaNasc");
+		
+		new ContatoDAO().inserir(new Contato(nome, email, endereco, dtaNasc));
 	
 	}
 }
